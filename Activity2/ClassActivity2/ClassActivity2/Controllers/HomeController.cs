9b0c4e278d04eeb8871495f798fcd467a5c26951
unknown <User@INFPE-45.up.ac.za>
2019-02-20 18:45:32 +0200

    my commit

---
+++ Activity2/ClassActivity2/ClassActivity2/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassActivity2.Models;
+using System.Data;
+using System.IO;
+using ClassActivity2.ViewModel;
+
+namespace ClassActivity2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+       
+
+        [HttpGet]
+        public ActionResult EmployeeReport()
+        {
+            ReportViewModel vm = new ReportViewModel();
+
+            vm.Employees = GetEmployee(0);
+
+            vm.DateFrom = new DateTime(2012,12,1);
+            vm.DateTo = new DateTime(2013, 12, 31);
+
+           return View(vm);
+
+        }
+        private SelectList GetEmployee(int selected)
+        {
+            using (HardwareDBEntities db = new HardwareDBEntities())
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+
+                var employees = db.lgemployees.Select(x => new SelectListItem
+                {
+                    Value = x.emp_num.ToString(),
+                    Text = x.emp_fname
+                }).ToList();
+                if (selected == 0)
+                    return new SelectList(employees, "Value", "Text");
+                else
+                    return new SelectList(employees, "Value", "Text",selected);
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Activity2/ClassActivity2/ClassActivity2/ViewModel/ReportViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClassActivity2.Models;
+using System.Web.Mvc;
+
+
+
+namespace ClassActivity2.ViewModel
+{
+    public class ReportViewModel
+    {
+        public IEnumerable<SelectListItem> Employees { get; set; }
+        public int SelectedEmployeeID { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+        public lgemployee employee { get; set; }
+        public lginvoice invoice { get; set; }
+
+        public List<IGrouping<string, ReportRecord>> results { get; set; }
+        public Dictionary<string, double> chartData { get; set; }
+
+        public class ReportRecord
+        {
+            public int employeeid { get; set;}
+
+            public string InvoiceDate { get; set;}
+
+            public double InvoiceTotal { get; set;}
+
+            public int invNum { get; set; }
+
+            public string EmployeeName { get; set; }
+
+            public string EmployeeSurname { get; set; }
+
+        }
+    }
+
+
+
+
+}
